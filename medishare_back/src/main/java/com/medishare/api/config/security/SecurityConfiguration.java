ddf949af27b8978996013733dd2873e9e29e27f1
kanghae05 <kanghae0510@gmail.com>
2026-08-10 17:05:58 +0900

    SecurityConfiguration 수정
    localhost 뿐만 아니라 모든 IP 및 포트의 프런트엔드 접속을 허용

--- medishare_back/src/main/java/com/medishare/api/config/security/SecurityConfiguration.java
+++ medishare_back/src/main/java/com/medishare/api/config/security/SecurityConfiguration.java
@@ -81,9 +81,7 @@ public class SecurityConfiguration {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
-        );
+        configuration.setAllowedOriginPatterns(List.of("*"));
 
         configuration.setAllowedMethods(
                 List.of("GET","POST","PUT","DELETE","OPTIONS")
