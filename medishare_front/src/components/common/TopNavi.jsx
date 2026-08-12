7e870964071f988a39e48e0ef8519fce5cdf6bad
sarangee22 <rrangee22222@naver.com>
2026-08-12 17:42:50 +0900

    Update TopNavi.jsx

--- medishare_front/src/components/common/TopNavi.jsx
+++ medishare_front/src/components/common/TopNavi.jsx
@@ -1,36 +1,24 @@
 import { useState } from "react";
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
 
-function TopNavi(){
-
+function TopNavi() {
   const [token, setToken] = useState(localStorage.getItem("token"));
-
-  // JWT 내부 정보
   const [login, setLogin] = useState(() => {
-    const data = localStorage.getItem("login"); // 문자열이므로
-    return data ? JSON.parse(data) : null; // JSON 데이터로 만들어 준다.
+    const data = localStorage.getItem("login");
+    return data ? JSON.parse(data) : null;
   });
- 
-  console.log("TopNavi login = " + login);
-  console.log(login?"TopNavi login.name = " + login.name:'없음');
 
-  // 로그아웃 처리
-  const logout = (e) => {
-    e.preventDefault();
-
-    // react에서만 token과 사용자 정보를 지운다.
+  const logout = (event) => {
+    event.preventDefault();
     localStorage.removeItem("token");
     localStorage.removeItem("login");
-
     setToken(null);
     setLogin(null);
-
     alert("로그아웃 되었습니다.");
-    // Navigate("/");
     location.href = "/";
-}
+  };
 
-  return(
+  return (
     <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
       <div className="container-fluid">
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
@@ -39,13 +27,30 @@ function TopNavi(){
         <div className="collapse navbar-collapse" id="mynavbar">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <NavLink to={"/"} className="nav-link">Home</NavLink>&nbsp;
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/d/dashboard" className="nav-link">
+                D Dashboard
+              </NavLink>
             </li>
             {token && (
-              <li className="nav-item">
-                <NavLink to="/report/list" className="nav-link">판독소견서</NavLink>
-              </li>
+              <>
+                <li className="nav-item">
+                  <NavLink to="/report/list" className="nav-link">
+                    판독소견
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/coop/received" className="nav-link">
+                    협진
+                  </NavLink>
+                </li>
+              </>
             )}
+<<<<<<< Updated upstream
             <li className="nav-item">
               <NavLink to="/notices" className="nav-link">공지사항</NavLink>
             </li>
@@ -53,46 +58,39 @@ function TopNavi(){
               <NavLink to="/special-cases" className="nav-link">특이케이스</NavLink>
             </li>
 
+=======
+>>>>>>> Stashed changes
           </ul>
           <ul className="navbar-nav ms-auto">
-              {!token && (
-                  <>
-                      <li className="nav-item">
-                          <Link className="nav-link" to="/member/login">
-                              Login
-                          </Link>
-                      </li>
+            {!token && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/member/login">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/member/write">
+                    Join
+                  </Link>
+                </li>
+              </>
+            )}
 
-                      <li className="nav-item">
-                          <Link className="nav-link" to="/member/write">
-                              Join
-                          </Link>
-                      </li>
-
-                      <li className="nav-item">
-                          <Link className="nav-link" to="/member/find-id">
-                              아이디찾기
-                          </Link>
-                      </li>
-                  </>
-              )}
-
-              {token && (
-                  <>
-                      <li className="nav-item">
-                          <Link className="nav-link" to="/member/logout" onClick={logout}>
-                              Logout
-                          </Link>
-                      </li>
-
-                      <li className="nav-item">
-                          <Link className="nav-link" to="/member/view">
-                              {login.name}
-                          </Link>
-                      </li>
-
-                  </>
-              )}
+            {token && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/member/logout" onClick={logout}>
+                    Logout
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/member/view">
+                    {login?.name || "사용자"}
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
