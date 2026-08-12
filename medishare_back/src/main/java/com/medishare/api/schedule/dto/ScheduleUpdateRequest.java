75d6a0570310efc48eb4742a2d21486a7e4cd30b
sarangee22 <rrangee22222@naver.com>
2026-08-12 17:45:32 +0900

    Create ScheduleUpdateRequest.java

---
+++ medishare_back/src/main/java/com/medishare/api/schedule/dto/ScheduleUpdateRequest.java
@@ -0,0 +1,28 @@
+package com.medishare.api.schedule.dto;
+
+import com.medishare.api.schedule.entity.ScheduleType;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor
+public class ScheduleUpdateRequest {
+
+    @NotNull
+    private LocalDate scheduleDate;
+
+    @NotNull
+    private LocalTime startTime;
+
+    @NotNull
+    private LocalTime endTime;
+
+    @NotNull
+    private ScheduleType scheduleType;
+
+    private String memo;
+}
