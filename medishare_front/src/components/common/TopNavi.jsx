dca3f1db73e9bc3463d237bbbc3e8283364f9234
hyebinlee-qp <wofhdenddl04@naver.com>
2026-08-13 15:49:07 +0900

    비로그인 시 헤더 숨김 설정

--- medishare_front/src/components/common/TopNavi.jsx
+++ medishare_front/src/components/common/TopNavi.jsx
@@ -21,7 +21,7 @@ function TopNavi() {
     localStorage.removeItem("login");
     setToken(null);
     setLogin(null);
-    alert("로그아웃 되었습니다.");
+    alert("로그아웃되었습니다.");
     location.href = "/";
   };
 
@@ -33,31 +33,13 @@ function TopNavi() {
         </button>
         <div className="collapse navbar-collapse" id="mynavbar">
           <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link">Home</NavLink>
-            </li>
-            {isLoggedIn && (
-              <li className="nav-item">
-                <NavLink to="/d/dashboard" className="nav-link">Dashboard</NavLink>
-              </li>
-            )}
-            {isAdmin && (
-              <li className="nav-item">
-                <NavLink to="/admin/medical-staff" className="nav-link">의료진 관리</NavLink>
-              </li>
-            )}
-            <li className="nav-item">
-              <NavLink to="/notices" className="nav-link">
-                공지사항
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/special-cases" className="nav-link">
-                특이케이스
-              </NavLink>
-            </li>
+            <li className="nav-item"><NavLink to="/" className="nav-link">Home</NavLink></li>
+            {isLoggedIn && <li className="nav-item"><NavLink to="/d/dashboard" className="nav-link">Dashboard</NavLink></li>}
+            {isAdmin && <li className="nav-item"><NavLink to="/admin/medical-staff" className="nav-link">의료진 관리</NavLink></li>}
             {isLoggedIn && (
               <>
+                <li className="nav-item"><NavLink to="/notices" className="nav-link">공지사항</NavLink></li>
+                <li className="nav-item"><NavLink to="/special-cases" className="nav-link">특이케이스</NavLink></li>
                 <li className="nav-item"><NavLink to="/report/list" className="nav-link">판독소견</NavLink></li>
                 <li className="nav-item"><NavLink to="/coop/received" className="nav-link">협진</NavLink></li>
               </>
